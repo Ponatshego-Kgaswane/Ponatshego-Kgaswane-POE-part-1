c53d91efa553d50ac1d444a5bf56d774d28be5dc
Ponatshego-Kgaswane <ST10439596@rcconnect.edu.za>
2024-11-21 15:31:51 +0200

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.login_project;
+import java.util.*;
+/**
+ *
+ * @author kgasw
+ */
+public class Task {
+      private String taskName;
+    private int taskNumber;
+    private String taskDescription;
+    private String developerDetails;
+    private int taskDuration;
+    private String taskStatus;
+    private String taskID;
+
+    // implementation of constructor
+    public Task(String taskName, int taskNumber, String taskDescription, String developerDetails, int taskDuration, String taskStatus) {
+        this.taskName = taskName;
+        this.taskNumber = taskNumber;
+        this.taskDescription = taskDescription;
+        this.developerDetails = developerDetails;
+        this.taskDuration = taskDuration;
+        this.taskStatus = taskStatus;
+        this.taskID = createTaskID();
+    }
+
+    // Method to Check task description length
+    public boolean checkTaskDescription() {
+        return taskDescription.length() <= 50;
+    }
+
+    // Generating of task ID
+    public String createTaskID() {
+        String taskInitials = taskName.substring(0, 2).toUpperCase();
+        String developerEnd = developerDetails.substring(developerDetails.length() - 3).toUpperCase();
+        return taskInitials + ":" + taskNumber + ":" + developerEnd;
+    }
+
+    // Method to print task details
+    public String printTaskDetails() {
+        return "Task Status: " + taskStatus + "\n" +
+               "Developer Details: " + developerDetails + "\n" +
+               "Task Number: " + taskNumber + "\n" +
+               "Task Name: " + taskName + "\n" +
+               "Task Description: " + taskDescription + "\n" +
+               "Task ID: " + taskID + "\n" +
+               "Task Duration: " + taskDuration + "hrs\n";
+    }
+
+    // Method to return task duration
+    public int returnTaskDuration() {
+        return taskDuration;
+    }
+
+// Getters
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public int getTaskNumber() {
+        return taskNumber;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public String getDeveloperDetails() {
+        return developerDetails;
+    }
+
+    public int getTaskDuration() {
+        return taskDuration;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+public String getTaskID() {
+        return taskID;
+    }
+}
